de7423aeaaed0a20633b7ba1979aacb3dcb56d88
Michael Matasyants <michael.matasyanc@gmail.com>
2023-08-15 18:01:36 +0300

    Use secelt realted to reduce number of queries

--- blog/views.py
+++ blog/views.py
@@ -49,7 +49,7 @@ def index(request):
 def post_detail(request, slug):
     posts_with_tags = Post.objects.fetch_tags()
     post = get_object_or_404(posts_with_tags, slug=slug)
-    comments = post.comments.prefetch_related('author')
+    comments = post.comments.select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
